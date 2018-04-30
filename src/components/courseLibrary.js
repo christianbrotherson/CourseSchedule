1386c41388deb38a97bd346e92b0de5ec3545981
Christian Brotherson <christian.brotherson@gmail.com>
2018-04-30 16:09:54 -0600

    added in add and remove buttons

--- src/components/courseLibrary.js
+++ src/components/courseLibrary.js
@@ -25,8 +25,8 @@ class CourseLibrary extends Component {
                         <div className={`course-check-mark ${course.enrolled ? 'show-content-fade' : 'hide-content-fade'}`}></div>
                     </div>
                     <a className={`course-arrow ${course.open ? null : 'course-arrow-close'}`} onClick={() => this.props.toggleDescription(course)}></a>
-                    <a className={`action ${course.enrolled ? 'hide-content' : 'show-content'}`} onClick={() => this.props.addCourse(course)}>Add</a>
-                    <a className={`action ${course.enrolled ? 'show-content' : 'hide-content'}`} onClick={() => this.props.removeCourse(course)}>Remove</a>
+                    <a className={`course-add action ${course.enrolled ? 'hide-content' : 'show-content'}`} onClick={() => this.props.addCourse(course)}></a>
+                    <a className={`course-remove action ${course.enrolled ? 'show-content' : 'hide-content'}`} onClick={() => this.props.removeCourse(course)}></a>
                 </div>
 
                 <AnimateHeight
